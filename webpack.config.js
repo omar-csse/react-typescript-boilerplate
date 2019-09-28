d1a948871ae8a0bcde91120093db58fce2e15e88
omar <omar.csse@gmail.com>
2019-09-28 19:51:20 +1000

    edit tsconfig,edit webpack.config to accept params

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,49 @@
-let path = require('path');
-let HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
-    entry: './src/main.tsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
-    },
-    devtool: "source-map",
-    mode: 'development',
-    module: {
-        rules: [{
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,    
-                loader: 'ts-loader',
-                options: { allowTsInNodeModules: true }
-            },
+module.exports = (env, argv) => {
+    return {
+        entry: './src/main.tsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.tsx', '.jsx'],
+            modules: ["src", "node_modules"],
+        },
+        devtool: "source-map",
+        mode: 'development',
+        module: {
+            rules: [{
+                    test: /\.ts(x?)$/,
+                    exclude: /node_modules/,
+                    loader: 'ts-loader',
+                    options: {
+                        allowTsInNodeModules: true
+                    }
+                },
 
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    loader: "source-map-loader"
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: __dirname + '/public/main.html',
+                filename: argv.mode == 'production' ? 'main.html' : 'index.html',
+                favicon: __dirname + '/public/favicon.ico'
+            })
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public/main.html'),
-            favicon: path.resolve(__dirname, 'public/favicon.ico')
-        })
-    ]
+    }
 }
